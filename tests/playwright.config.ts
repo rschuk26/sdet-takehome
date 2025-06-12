ff21b0968608e19f2db71d9664e75d2ed898104c
rsukr26 <slusarchuk.roman@gmail.com>
2025-06-12 15:06:10 -0400

    test run config added

--- tests/playwright.config.ts
+++ tests/playwright.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-    testDir: './tests',
+    testDir: './',
     reporter: [
         ['list'],
         ['html', { open: 'always', outputFolder: 'playwright-report' }]
